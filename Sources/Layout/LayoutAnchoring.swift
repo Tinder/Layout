8ceae86c6bc12671b9285658bf3f3d096af6cf0e
Christopher Fuller <christopher.fuller@gotinder.com>
2023-02-17 12:30:24 -0800

    Add Layout

---
+++ Sources/Layout/LayoutAnchoring.swift
@@ -0,0 +1,140 @@
+//
+//  LayoutAnchoring.swift
+//  Layout
+//
+//  Created by Christopher Fuller on 2/17/23.
+//
+
+import UIKit
+
+public protocol LayoutAnchoring {
+
+    var left: NSLayoutXAxisAnchor { get }
+    var centerX: NSLayoutXAxisAnchor { get }
+    var right: NSLayoutXAxisAnchor { get }
+    var leading: NSLayoutXAxisAnchor { get }
+    var trailing: NSLayoutXAxisAnchor { get }
+
+    var top: NSLayoutYAxisAnchor { get }
+    var centerY: NSLayoutYAxisAnchor { get }
+    var firstBaseline: NSLayoutYAxisAnchor { get }
+    var bottom: NSLayoutYAxisAnchor { get }
+    var lastBaseline: NSLayoutYAxisAnchor { get }
+
+    var width: NSLayoutDimension { get }
+    var height: NSLayoutDimension { get }
+}
+
+extension UIView: LayoutAnchoring {
+
+    public var left: NSLayoutXAxisAnchor { leftAnchor }
+    public var centerX: NSLayoutXAxisAnchor { centerXAnchor }
+    public var right: NSLayoutXAxisAnchor { rightAnchor }
+    public var leading: NSLayoutXAxisAnchor { leadingAnchor }
+    public var trailing: NSLayoutXAxisAnchor { trailingAnchor }
+
+    public var top: NSLayoutYAxisAnchor { topAnchor }
+    public var centerY: NSLayoutYAxisAnchor { centerYAnchor }
+    public var firstBaseline: NSLayoutYAxisAnchor { firstBaselineAnchor }
+    public var lastBaseline: NSLayoutYAxisAnchor { lastBaselineAnchor }
+    public var bottom: NSLayoutYAxisAnchor { bottomAnchor }
+
+    public var width: NSLayoutDimension { widthAnchor }
+    public var height: NSLayoutDimension { heightAnchor }
+}
+
+extension UILayoutGuide: LayoutAnchoring {
+
+    public var left: NSLayoutXAxisAnchor { leftAnchor }
+    public var centerX: NSLayoutXAxisAnchor { centerXAnchor }
+    public var right: NSLayoutXAxisAnchor { rightAnchor }
+    public var leading: NSLayoutXAxisAnchor { leadingAnchor }
+    public var trailing: NSLayoutXAxisAnchor { trailingAnchor }
+
+    public var top: NSLayoutYAxisAnchor { topAnchor }
+    public var centerY: NSLayoutYAxisAnchor { centerYAnchor }
+    public var firstBaseline: NSLayoutYAxisAnchor { fatalError("firstBaseline does not exist on UILayoutGuide") }
+    public var lastBaseline: NSLayoutYAxisAnchor { fatalError("lastBaseline does not exist on UILayoutGuide") }
+    public var bottom: NSLayoutYAxisAnchor { bottomAnchor }
+
+    public var width: NSLayoutDimension { widthAnchor }
+    public var height: NSLayoutDimension { heightAnchor }
+}
+
+extension UILayoutSupport {
+
+    public var top: NSLayoutYAxisAnchor { topAnchor }
+    public var bottom: NSLayoutYAxisAnchor { bottomAnchor }
+    public var height: NSLayoutDimension { heightAnchor }
+}
+
+extension NSLayoutXAxisAnchor {
+
+    public func constraint(is relation: ConstraintRelation = .equal,
+                           to anchor: NSLayoutXAxisAnchor,
+                           constant: CGFloat = 0) -> NSLayoutConstraint {
+        switch relation {
+        case .equal:
+            return constraint(equalTo: anchor, constant: constant)
+        case .greaterThanOrEqual:
+            return constraint(greaterThanOrEqualTo: anchor, constant: constant)
+        case .lessThanOrEqual:
+            return constraint(lessThanOrEqualTo: anchor, constant: constant)
+        }
+    }
+}
+
+extension NSLayoutYAxisAnchor {
+
+    public func constraint(is relation: ConstraintRelation = .equal,
+                           to anchor: NSLayoutYAxisAnchor,
+                           constant: CGFloat = 0) -> NSLayoutConstraint {
+        switch relation {
+        case .equal:
+            return constraint(equalTo: anchor, constant: constant)
+        case .greaterThanOrEqual:
+            return constraint(greaterThanOrEqualTo: anchor, constant: constant)
+        case .lessThanOrEqual:
+            return constraint(lessThanOrEqualTo: anchor, constant: constant)
+        }
+    }
+}
+
+extension NSLayoutDimension {
+
+    public func constraint(is relation: ConstraintRelation = .equal,
+                           to anchor: NSLayoutDimension,
+                           constant: CGFloat = 0) -> NSLayoutConstraint {
+        switch relation {
+        case .equal:
+            return constraint(equalTo: anchor, constant: constant)
+        case .greaterThanOrEqual:
+            return constraint(greaterThanOrEqualTo: anchor, constant: constant)
+        case .lessThanOrEqual:
+            return constraint(lessThanOrEqualTo: anchor, constant: constant)
+        }
+    }
+
+    public func constraint(is relation: ConstraintRelation = .equal,
+                           _ constant: CGFloat) -> NSLayoutConstraint {
+        switch relation {
+        case .equal:
+            return constraint(equalToConstant: constant)
+        case .greaterThanOrEqual:
+            return constraint(greaterThanOrEqualToConstant: constant)
+        case .lessThanOrEqual:
+            return constraint(lessThanOrEqualToConstant: constant)
+        }
+    }
+}
+
+extension UIViewController {
+
+    public var safeTop: NSLayoutYAxisAnchor {
+        view.safeAreaLayoutGuide.top
+    }
+
+    public var safeBottom: NSLayoutYAxisAnchor {
+        view.safeAreaLayoutGuide.bottom
+    }
+}
---
+++ Sources/Layout/NSLayoutConstraint+Layout.swift
@@ -0,0 +1,151 @@
+//
+//  NSLayoutConstraint+Layout.swift
+//  Layout
+//
+//  Created by Christopher Fuller on 2/17/23.
+//
+
+import UIKit
+
+extension UILayoutPriority {
+
+    public static let disabled: UILayoutPriority = .init(0)
+    public static let low: UILayoutPriority = .defaultLow
+    public static let high: UILayoutPriority = .defaultHigh
+
+    public static func + (priority: UILayoutPriority, offset: Float) -> UILayoutPriority {
+        let priority: Float = priority.rawValue + offset
+        return UILayoutPriority(min(max(priority, 0), 1_000))
+    }
+
+    public static func - (priority: UILayoutPriority, offset: Float) -> UILayoutPriority {
+        priority + -offset
+    }
+
+    public static func += (priority: inout UILayoutPriority, offset: Float) {
+        priority = priority + offset
+    }
+
+    public static func -= (priority: inout UILayoutPriority, offset: Float) {
+        priority = priority - offset
+    }
+}
+
+extension NSLayoutConstraint {
+
+    // MARK: - VFL Convenience APIs
+
+    public static func constraints(format: String,
+                                   views: [String: Any],
+                                   metrics: [String: Any]? = nil,
+                                   options: NSLayoutConstraint.FormatOptions = []) -> [NSLayoutConstraint] {
+
+        NSLayoutConstraint.constraints(withVisualFormat: format,
+                                       options: options,
+                                       metrics: metrics,
+                                       views: views)
+    }
+
+    public static func constraints(formats: [String],
+                                   views: [String: Any],
+                                   metrics: [String: Any]? = nil,
+                                   options: NSLayoutConstraint.FormatOptions = []) -> [NSLayoutConstraint] {
+        formats.flatMap {
+            constraints(format: $0, views: views, metrics: metrics, options: options)
+        }
+    }
+
+    // MARK: - Mutators
+
+    @discardableResult
+    public func activate() -> NSLayoutConstraint {
+        isActive = true
+        return self
+    }
+
+    @discardableResult
+    public func deactivate() -> NSLayoutConstraint {
+        isActive = false
+        return self
+    }
+
+    @discardableResult
+    public func offsetBy(_ constant: CGFloat) -> NSLayoutConstraint {
+        self.constant = constant
+        return self
+    }
+
+    @discardableResult
+    public func withPriority(_ priority: UILayoutPriority) -> NSLayoutConstraint {
+        self.priority = priority
+        return self
+    }
+
+    @discardableResult
+    public func require() -> NSLayoutConstraint {
+        withPriority(.required)
+    }
+
+    @discardableResult
+    public func lowPriority() -> NSLayoutConstraint {
+        withPriority(.low)
+    }
+
+    @discardableResult
+    public func highPriority() -> NSLayoutConstraint {
+        withPriority(.high)
+    }
+}
+
+// MARK: - NSLayoutConstraint Array Helpers
+
+extension Array where Element == NSLayoutConstraint {
+
+    @discardableResult
+    public func activate() -> [NSLayoutConstraint] {
+        NSLayoutConstraint.activate(self)
+        return self
+    }
+
+    @discardableResult
+    public func deactivate() -> [NSLayoutConstraint] {
+        NSLayoutConstraint.deactivate(self)
+        return self
+    }
+
+    @discardableResult
+    public func withPriority(_ priority: UILayoutPriority) -> [NSLayoutConstraint] {
+        map { $0.withPriority(priority) }
+    }
+
+    public func prioritize(_ priority: UILayoutPriority) {
+        forEach { $0.priority = priority }
+    }
+}
+
+// KLW [2/1/19]: Once UIView+Deprecated.swift no longer needs this - can be made internal again
+public extension NSLayoutConstraint.Axis {
+
+    var orientation: String {
+        switch self {
+        case .vertical:
+            return "V"
+
+        case .horizontal:
+            return "H"
+        @unknown default:
+            return "H"
+        }
+    }
+
+    var centerAttribute: NSLayoutConstraint.Attribute {
+        switch self {
+        case .horizontal:
+            return .centerX
+        case .vertical:
+            return .centerY
+        @unknown default:
+            return .centerX
+        }
+    }
+}
