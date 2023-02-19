bcb59bc137cef51925ffcec5491294eeb58461db
Christopher Fuller <christopher.fuller@gotinder.com>
2023-02-19 07:19:08 -0800

    Add layout factory for a single item

--- Sources/Layout/UIView+Layout.swift
+++ Sources/Layout/UIView+Layout.swift
@@ -9,6 +9,40 @@ import UIKit
 
 extension UIView {
 
+    /// Adds a subview and constraints to a view.
+    ///
+    /// - Note:
+    ///     * Views are added to view hierarchy in reverse order. The last item in layout call will be at the front.
+    ///     * Must call `.activate()` for constraints to become active.
+    ///     * Activating/Deactivating an instance will only affect the constraints attached to that instance.
+    ///     * Deactivating the instance does not remove or hide views.
+    ///     * A view can have multiple Layout instances, useful for when a view has different states.
+    ///     * Preferred format for adding one view with one constraint
+    ///         ````
+    ///         view1.layout(view2.pin()).activate()
+    ///         ````
+    ///     * Preferred format for adding multiple views or constraints
+    ///         ````
+    ///         view1.layout {
+    ///             view2
+    ///               .to([.leading, .top, .trailing])
+    ///             view3
+    ///               .pad()
+    ///               .to(.bottom)
+    ///         }
+    ///         .vertical([view2, view3])
+    ///         .activate()
+    ///         ````
+    /// - Parameters:
+    ///   - metrics: (optional) metrics for VFL
+    ///   - item: item to be included in Layout
+    public func layout(
+        metrics: [String: Any] = [:],
+        _ item: LayoutItem
+    ) -> Layout {
+        Layout(self, metrics: metrics, items: [item])
+    }
+
     /// Adds subviews and constraints to a view.
     ///
     /// - Note:
@@ -23,13 +57,13 @@ extension UIView {
     ///         ````
     ///     * Preferred format for adding multiple views or constraints
     ///         ````
-    ///         view1.layout(
+    ///         view1.layout {
     ///             view2
-    ///               .to([.leading, .top, .trailing]),
+    ///               .to([.leading, .top, .trailing])
     ///             view3
     ///               .pad()
     ///               .to(.bottom)
-    ///         )
+    ///         }
     ///         .vertical([view2, view3])
     ///         .activate()
     ///         ````
