6af3481135f38466c05f184ff606fbb59d4f46b8
Christopher Fuller <christopher.fuller@gotinder.com>
2023-02-27 15:42:18 -0800

    Add Nimble dependency (#26)
    * Add Nimble dependency
    
    * Add Nimble dependency to podspec

--- Package.swift
+++ Package.swift
@@ -9,6 +9,11 @@ let package = Package(
             name: "Layout",
             targets: ["Layout"]),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Quick/Nimble.git",
+            from: "10.0.0"),
+    ],
     targets: [
         .target(
             name: "Layout"),
@@ -16,6 +21,7 @@ let package = Package(
             name: "LayoutTests",
             dependencies: [
                 "Layout",
+                "Nimble",
             ]),
     ]
 )
