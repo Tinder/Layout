17325dfbafc7086917892b5db00215b7c1d2f805
Christopher Fuller <christopher.fuller@gotinder.com>
2023-03-01 09:54:00 -0800

    Add ViewImageConfig extension for iPhone13/Mini

--- Tests/LayoutTests/SnapshotTesting.swift
+++ Tests/LayoutTests/SnapshotTesting.swift
@@ -34,3 +34,43 @@ internal func assertLayout(
                        line: line)
     }
 }
+
+
+/*
+ Reference:
+ https://github.com/pointfreeco/swift-snapshot-testing/blob/main/Sources/SnapshotTesting/Common/View.swift
+ */
+extension ViewImageConfig {
+
+    internal static let iPhone13: ViewImageConfig = .iPhone13(.portrait)
+
+    internal static func iPhone13(_ orientation: Orientation) -> ViewImageConfig {
+        let safeArea: UIEdgeInsets
+        let size: CGSize
+        switch orientation {
+        case .landscape:
+            safeArea = .init(top: 0, left: 47, bottom: 21, right: 47)
+            size = .init(width: 844, height: 390)
+        case .portrait:
+            safeArea = .init(top: 47, left: 0, bottom: 34, right: 0)
+            size = .init(width: 390, height: 844)
+        }
+        return ViewImageConfig(safeArea: safeArea, size: size, traits: .iPhone13(orientation))
+    }
+
+    internal static let iPhone13Mini: ViewImageConfig = .iPhone13Mini(.portrait)
+
+    internal static func iPhone13Mini(_ orientation: Orientation) -> ViewImageConfig {
+        let safeArea: UIEdgeInsets
+        let size: CGSize
+        switch orientation {
+        case .landscape:
+            safeArea = .init(top: 0, left: 50, bottom: 21, right: 50)
+            size = .init(width: 812, height: 375)
+        case .portrait:
+            safeArea = .init(top: 50, left: 0, bottom: 34, right: 0)
+            size = .init(width: 375, height: 812)
+        }
+        return ViewImageConfig(safeArea: safeArea, size: size, traits: .iPhone13(orientation))
+    }
+}
