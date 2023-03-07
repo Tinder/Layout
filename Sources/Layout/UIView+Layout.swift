bbd1f288baf5782a7fa33f4d598bdc1053b0f8f3
Christopher Fuller <christopher.fuller@gotinder.com>
2023-03-07 07:06:38 -0800

    Add UIView extension layout method without items

--- Sources/Layout/UIView+Layout.swift
+++ Sources/Layout/UIView+Layout.swift
@@ -9,6 +9,37 @@ import UIKit
 
 extension UIView {
 
+    /// Adds constraints to a view.
+    ///
+    /// - Note:
+    ///     * Views are added to view hierarchy in reverse order. The last item in layout call will be at the front.
+    ///     * Must call `.activate()` for constraints to become active.
+    ///     * Activating/Deactivating an instance will only affect the constraints attached to that instance.
+    ///     * Deactivating the instance does not remove or hide views.
+    ///     * A view can have multiple Layout instances, useful for when a view has different states.
+    ///     * Preferred format for adding one view with one constraint
+    ///         ````
+    ///         view1.layout(view2.pin()).activate()
+    ///         ````
+    ///     * Preferred format for adding multiple views or constraints
+    ///         ````
+    ///         view1.layout {
+    ///             view2
+    ///               .to([.leading, .top, .trailing])
+    ///             view3
+    ///               .pad()
+    ///               .to(.bottom)
+    ///         }
+    ///         .vertical([view2, view3])
+    ///         .activate()
+    ///         ````
+    /// - Parameter metrics: (optional) metrics for VFL
+    public func layout(
+        metrics: [String: Any] = [:]
+    ) -> Layout {
+        Layout(self, metrics: metrics, items: [])
+    }
+
     /// Adds a subview and constraints to a view.
     ///
     /// - Note:
