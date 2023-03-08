b61338b8239093d8b84f3e0c2d706ed01093d27b
Christopher Fuller <christopher.fuller@gotinder.com>
2023-03-08 11:42:32 -0800

    Improve testing support (#36)
    * Improve testing support
    
    * Add make recipe to delete snapshots

---
+++ Tests/LayoutTests/Support/Views.swift
@@ -0,0 +1,78 @@
+//
+//  Views.swift
+//  LayoutTests
+//
+//  Created by Christopher Fuller on 3/7/23.
+//
+
+import UIKit
+
+internal var pinkView: UIView {
+    view(with: UIColor(red: 1, green: 0.176471, blue: 0.333333, alpha: 1))
+}
+
+internal var blackView: UIView {
+    view(with: .black)
+}
+
+internal var darkGrayView: UIView {
+    view(with: .darkGray)
+}
+
+internal var lightGrayView: UIView {
+    view(with: .lightGray)
+}
+
+internal var whiteView: UIView {
+    view(with: .white)
+}
+
+internal var grayView: UIView {
+    view(with: .gray)
+}
+
+internal var redView: UIView {
+    view(with: .red)
+}
+
+internal var greenView: UIView {
+    view(with: .green)
+}
+
+internal var blueView: UIView {
+    view(with: .blue)
+}
+
+internal var cyanView: UIView {
+    view(with: .cyan)
+}
+
+internal var yellowView: UIView {
+    view(with: .yellow)
+}
+
+internal var magentaView: UIView {
+    view(with: .magenta)
+}
+
+internal var orangeView: UIView {
+    view(with: .orange)
+}
+
+internal var purpleView: UIView {
+    view(with: .purple)
+}
+
+internal var brownView: UIView {
+    view(with: .brown)
+}
+
+internal var clearView: UIView {
+    view(with: .clear)
+}
+
+private func view(with backgroundColor: UIColor) -> UIView {
+    let view: UIView = .init()
+    view.backgroundColor = backgroundColor
+    return view
+}
