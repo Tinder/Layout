56cd0c46b9f5150f29fb6895dbc9722df086b39b
Christopher Fuller <christopher.fuller@gotinder.com>
2023-03-08 12:02:47 -0800

    Add layout item snapshot tests (w/ recursive desc)

---
+++ Tests/LayoutTests/LayoutItemSnapshotTests.swift
@@ -0,0 +1,24 @@
+//
+//  LayoutItemSnapshotTests.swift
+//  LayoutTests
+//
+//  Created by Christopher Fuller on 3/7/23.
+//
+
+@testable import Layout
+import XCTest
+
+final class LayoutItemSnapshotTests: XCTestCase {
+
+    func testSize() {
+        assertLayout { view in
+            view.layout(pinkView.size(width: 100, height: 200))
+        }
+    }
+
+    func testSizeWithCGSize() {
+        assertLayout { view in
+            view.layout(pinkView.size(CGSize(width: 100, height: 200)))
+        }
+    }
+}
