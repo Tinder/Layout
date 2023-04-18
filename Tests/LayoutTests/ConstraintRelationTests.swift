9d99df9a7a3ab12d8e149ff9b246fdfdca540c71
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-04-18 11:41:31 -0700

    Add ConstraintRelationTests (#43)
    * Add ConstraintRelationTests testInitWithRelation
    
    * Improve test
    
    * Improve test
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    ---------
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

---
+++ Tests/LayoutTests/ConstraintRelationTests.swift
@@ -0,0 +1,19 @@
+//
+//  ConstraintRelationTests.swift
+//  LayoutTests
+//
+//  Created by Garric Nahapetian on 4/11/23.
+//
+
+@testable import Layout
+import Nimble
+import XCTest
+
+final class ConstraintRelationTests: XCTestCase {
+
+    func testLayoutConstraintRelationInitializer() {
+        expect(NSLayoutConstraint.Relation(relation: .equal)) == .equal
+        expect(NSLayoutConstraint.Relation(relation: .greaterThanOrEqual)) == .greaterThanOrEqual
+        expect(NSLayoutConstraint.Relation(relation: .lessThanOrEqual)) == .lessThanOrEqual
+    }
+}
