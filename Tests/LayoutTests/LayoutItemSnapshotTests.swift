9efa655aaf237ab56bdf81730d2184683f6fcccb
Garric Nahapetian <garric.nahapetian@gotinder.com>
2023-04-24 17:37:41 -0700

    Add testViewLayoutItemLayoutAnchoring

--- Tests/LayoutTests/LayoutItemSnapshotTests.swift
+++ Tests/LayoutTests/LayoutItemSnapshotTests.swift
@@ -3,6 +3,7 @@
 //
 
 @testable import Layout
+import Nimble
 import XCTest
 
 final class LayoutItemSnapshotTests: XCTestCase {
@@ -17,5 +18,31 @@ final class LayoutItemSnapshotTests: XCTestCase {
         assertLayout { view in
             view.layout(pinkView.size(CGSize(width: 100, height: 200)))
         }
+    }
+
+    func testViewLayoutItemLayoutAnchoring() {
+
+        // GIVEN
+
+        let view: UIView = pinkView
+
+        // WHEN
+
+        let item: ViewLayoutItem = .init(layoutItemView: view) { _ in [] }
+
+        // THEN
+
+        expect(item.left) == view.left
+        expect(item.centerX) == view.centerX
+        expect(item.right) == view.right
+        expect(item.leading) == view.leading
+        expect(item.trailing) == view.trailing
+        expect(item.top) == view.top
+        expect(item.centerY) == view.centerY
+        expect(item.firstBaseline) == view.firstBaseline
+        expect(item.bottom) == view.bottom
+        expect(item.lastBaseline) == view.lastBaseline
+        expect(item.width) == view.width
+        expect(item.height) == view.height
     }
 }
