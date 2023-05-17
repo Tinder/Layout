0f53953198e013dfff2bc150649334fee8c00314
Garric Nahapetian <garric.nahapetian@gotinder.com>
2023-05-16 18:14:38 -0700

    Add toGuides(insets:priority:) tests

--- Tests/LayoutTests/LayoutItemTests.swift
+++ Tests/LayoutTests/LayoutItemTests.swift
@@ -23,4 +23,35 @@ final class LayoutItemTests: XCTestCase {
             view.layout(pinkView.size(CGSize(width: 100, height: 200)))
         }
     }
+
+    func testToGuides() {
+        assertLayout { view in
+            view.layout(pinkView.toGuides())
+        }
+    }
+
+    func testToGuidesInsets() {
+        assertLayout { view in
+            view.layout(pinkView.toGuides(insets: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)))
+        }
+    }
+
+    func testToGuidesInsetPriority() {
+
+        // GIVEN
+
+        let pinkView: UIView = pinkView
+        let blueView: UIView = blueView
+
+        // THEN
+
+        assertLayout { view in
+            view.layout {
+                pinkView.toGuides(insets: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10), priority: .low)
+                pinkView.toGuides(priority: .high)
+                blueView.toGuides(priority: .low)
+                blueView.toGuides(insets: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10), priority: .high)
+            }
+        }
+    }
 }
