419241ff01ac2685a56e036c251b2cd43dad7a1e
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-06-15 15:31:56 -0700

    Add testViewLayoutItemLayoutAnchoring (#65)
    * Add testViewLayoutItemLayoutAnchoring
    
    * Change order
    
    * Improve test
    
    * Improve test implementation
    
    * Resolve package

--- Tests/LayoutTests/LayoutItemTests.swift
+++ Tests/LayoutTests/LayoutItemTests.swift
@@ -364,6 +364,28 @@ final class LayoutItemTests: XCTestCase {
         }
     }
 
+    func testViewLayoutItemLayoutAnchoring() {
+
+        // GIVEN
+
+        let item: ViewLayoutItem = .init(layoutItemView: UIView()) { _ in [] }
+
+        // THEN
+
+        expect(item.left) == item.layoutItemView.left
+        expect(item.centerX) == item.layoutItemView.centerX
+        expect(item.right) == item.layoutItemView.right
+        expect(item.leading) == item.layoutItemView.leading
+        expect(item.trailing) == item.layoutItemView.trailing
+        expect(item.top) == item.layoutItemView.top
+        expect(item.centerY) == item.layoutItemView.centerY
+        expect(item.firstBaseline) == item.layoutItemView.firstBaseline
+        expect(item.lastBaseline) == item.layoutItemView.lastBaseline
+        expect(item.bottom) == item.layoutItemView.bottom
+        expect(item.width) == item.layoutItemView.width
+        expect(item.height) == item.layoutItemView.height
+    }
+
     func testLayoutConstraintAttributeArrayExtension() {
         expect([NSLayoutConstraint.Attribute].edges) == [.top, .left, .bottom, .right]
         expect([NSLayoutConstraint.Attribute].vertical) == [.top, .bottom]
