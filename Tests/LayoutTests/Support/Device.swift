38d70a38fc32a3e7a534607d2499e2934a4c4c0c
Garric Nahapetian <garric.nahapetian@gotinder.com>
2023-07-26 19:09:22 -0700

    Add landscape test devices

--- Tests/LayoutTests/Support/Device.swift
+++ Tests/LayoutTests/Support/Device.swift
@@ -7,13 +7,63 @@
 //  See https://github.com/Tinder/Layout/blob/main/LICENSE for license information.
 //
 
-internal enum Device: CaseIterable, CustomStringConvertible {
+internal enum Device: CustomStringConvertible {
 
-    case iPhone8
-    case iPhoneSE
-    case iPhoneX
-    case iPhone13
-    case iPhone13mini
+    case iPhone8(Orientation)
+    case iPhoneSE(Orientation)
+    case iPhoneX(Orientation)
+    case iPhone13(Orientation)
+    case iPhone13mini(Orientation)
+
+    internal enum Orientation {
+
+        case portrait
+        case landscape
+    }
+
+    internal static var iPhone8: [Device] {
+        [.iPhone8(.portrait), .iPhone8(.landscape)]
+    }
+
+    internal static var iPhoneSE: [Device] {
+        [.iPhoneSE(.portrait), .iPhoneSE(.landscape)]
+    }
+
+    internal static var iPhoneX: [Device] {
+        [.iPhoneX(.portrait), .iPhoneX(.landscape)]
+    }
+
+    internal static var iPhone13: [Device] {
+        [.iPhone13(.portrait), .iPhone13(.landscape)]
+    }
+
+    internal static var iPhone13mini: [Device] {
+        [.iPhone13mini(.portrait), .iPhone13mini(.landscape)]
+    }
+
+    internal static var allTestDevices: [Device] {
+        portraitTestDevices + landscapeTestDevices
+    }
+
+    internal static var portraitTestDevices: [Device] {
+        [
+            .iPhone8(.portrait),
+            .iPhoneSE(.portrait),
+            .iPhoneX(.portrait),
+            .iPhone13(.portrait),
+            .iPhone13mini(.portrait)
+        ]
+    }
+
+    internal static var landscapeTestDevices: [Device] {
+        [
+            .iPhone8(.landscape),
+            .iPhoneSE(.landscape),
+            .iPhoneX(.landscape),
+            .iPhone13(.landscape),
+            .iPhone13mini(.landscape)
+        ]
+    }
 
     internal var description: String {
         name
@@ -21,16 +71,21 @@ internal enum Device: CaseIterable, CustomStringConvertible {
 
     internal var name: String {
         switch self {
-        case .iPhone8:
-            return "iPhone 8"
-        case .iPhoneSE:
-            return "iPhone SE"
-        case .iPhoneX:
-            return "iPhone X"
-        case .iPhone13:
-            return "iPhone 13"
-        case .iPhone13mini:
-            return "iPhone 13 mini"
+        case let .iPhone8(orientation):
+            let name: String = "iPhone 8"
+            return orientation == .portrait ? name : "\(name) - \(orientation)"
+        case let .iPhoneSE(orientation):
+            let name: String = "iPhone SE"
+            return orientation == .portrait ? name : "\(name) - \(orientation)"
+        case let .iPhoneX(orientation):
+            let name: String = "iPhone X"
+            return orientation == .portrait ? name : "\(name) - \(orientation)"
+        case let .iPhone13(orientation):
+            let name: String = "iPhone 13"
+            return orientation == .portrait ? name : "\(name) - \(orientation)"
+        case let .iPhone13mini(orientation):
+            let name: String = "iPhone 13 mini"
+            return orientation == .portrait ? name : "\(name) - \(orientation)"
         }
     }
 }
