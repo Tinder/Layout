cf1c2a7196e74adabbfd1b6a43c55f3838c11529
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-10-16 14:51:47 -0700

    Add UIEdgesInsets extension (#148)
    * Add UIEdgeInsets extension
    
    * Improve test name
    
    * Fix order
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    ---------
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

---
+++ Tests/LayoutTests/UIEdgeInsetsTests.swift
@@ -0,0 +1,48 @@
+//
+//  All Contributions by Match Group
+//
+//  Copyright © 2023 Tinder (Match Group, LLC)
+//
+//  Licensed under the Match Group Modified 3-Clause BSD License.
+//  See https://github.com/Tinder/Layout/blob/main/LICENSE for license information.
+//
+
+@testable import Layout
+import Nimble
+import UIKit
+import XCTest
+
+final class UIEdgeInsetsTests: XCTestCase {
+
+    func testInitWithInset() {
+
+        // GIVEN
+
+        let inset: CGFloat = 5
+
+        // WHEN
+
+        let insets: UIEdgeInsets = .init(inset)
+
+        // THEN
+
+        expect(insets.top) == inset
+        expect(insets.left) == inset
+        expect(insets.bottom) == inset
+        expect(insets.right) == inset
+    }
+
+    func testConstantForEdge() {
+
+        // GIVEN
+
+        let insets: UIEdgeInsets = .init(top: 1, left: 2, bottom: 3, right: 4)
+
+        // THEN
+
+        expect(insets.constant(for: .top)) == 1
+        expect(insets.constant(for: .left)) == 2
+        expect(insets.constant(for: .bottom)) == -3
+        expect(insets.constant(for: .right)) == -4
+    }
+}
