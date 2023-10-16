a5a3a29bb98d8ab2c934c0da5e1372c89219d04e
tinder-emanharoutunian <eman.haroutunian@gotinder.com>
2023-10-16 14:23:17 -0700

    Add NSDirectionalEdgeInset extension and tests

---
+++ Tests/LayoutTests/NSDirectionalEdgeInsetsTests.swift
@@ -0,0 +1,47 @@
+//
+//  All Contributions by Match Group
+//
+//  Copyright © 2023 Tinder (Match Group, LLC)
+//
+//  Licensed under the Match Group Modified 3-Clause BSD License.
+//  See https://github.com/Tinder/Layout/blob/main/LICENSE for license information.
+//
+
+@testable import Layout
+import Nimble
+import XCTest
+
+final class NSDirectionalEdgeInsetsTests: XCTestCase {
+
+    func testInitInset() {
+
+        // GIVEN
+
+        let inset: CGFloat = 5
+
+        // WHEN
+
+        let insets: NSDirectionalEdgeInsets = .init(inset)
+
+        // THEN
+
+        expect(insets.top) == inset
+        expect(insets.bottom) == inset
+        expect(insets.leading) == inset
+        expect(insets.trailing) == inset
+    }
+
+    func testConstantForEdge() {
+
+        // GIVEN
+
+        let insets: NSDirectionalEdgeInsets = .init(top: 1, leading: 2, bottom: 3, trailing: 4)
+
+        // THEN
+
+        expect(insets.constant(for: .top)) == 1
+        expect(insets.constant(for: .bottom)) == -3
+        expect(insets.constant(for: .leading)) == 2
+        expect(insets.constant(for: .trailing)) == -4
+    }
+}
