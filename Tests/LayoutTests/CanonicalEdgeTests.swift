a68f675ed9c00d4c8a63e4df9327726656305cd4
tinder-emanharoutunian <eman.haroutunian@gotinder.com>
2023-10-16 12:44:33 -0700

    Add CanonicalEdge.swift and test

---
+++ Tests/LayoutTests/CanonicalEdgeTests.swift
@@ -0,0 +1,19 @@
+//
+//  All Contributions by Match Group
+//
+//  Copyright © 2023 Tinder (Match Group, LLC)
+//
+//  Licensed under the Match Group Modified 3-Clause BSD License.
+//  See https://github.com/Tinder/Layout/blob/main/LICENSE for license information.
+//
+
+@testable import Layout
+import Nimble
+import XCTest
+
+final class CanonicalEdgeTests: XCTestCase {
+
+    func testCanonicalEdgeAllCases() {
+        expect(CanonicalEdge.allCases) == [.top, .bottom, .left, .right]
+    }
+}
