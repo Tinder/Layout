caf3339701b1d7448ef3e419f3ebfd0519c7c7a3
Garric Nahapetian <garric.nahapetian@gotinder.com>
2023-10-16 14:56:54 -0700

    Add private to method

--- Sources/Layout/LayoutItem.swift
+++ Sources/Layout/LayoutItem.swift
@@ -552,6 +552,45 @@ extension LayoutItem {
     }
 
     // swiftlint:enable anonymous_argument_in_multiline_closure
+
+    private func to(
+        _ edge: CanonicalEdge,
+        of boundary: LayoutBoundary,
+        insets: UIEdgeInsets,
+        priority: UILayoutPriority
+    ) -> NSLayoutConstraint {
+        switch edge {
+        case .top:
+            return layoutItemView
+                .top
+                .constraint(equalTo: boundary.top, constant: insets.constant(for: .top))
+                .withPriority(priority)
+        case .bottom:
+            return layoutItemView
+                .bottom
+                .constraint(equalTo: boundary.bottom, constant: insets.constant(for: .bottom))
+                .withPriority(priority)
+        case .left:
+            return layoutItemView
+                .left
+                .constraint(equalTo: boundary.left, constant: insets.constant(for: .left))
+                .withPriority(priority)
+        case .right:
+            return layoutItemView
+                .right
+                .constraint(equalTo: boundary.right, constant: insets.constant(for: .right))
+                .withPriority(priority)
+        }
+    }
+
+    private func to(
+        _ edge: CanonicalEdge,
+        of boundary: LayoutBoundary,
+        inset: CGFloat,
+        priority: UILayoutPriority
+    ) -> NSLayoutConstraint {
+        to(edge, of: boundary, insets: UIEdgeInsets(inset), priority: priority)
+    }
 }
 
 // swiftlint:disable:next no_grouping_extension
