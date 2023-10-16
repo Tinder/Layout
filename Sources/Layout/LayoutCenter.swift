9952256d326a117cf6a67359b56fe813014ff46a
tinder-emanharoutunian <eman.haroutunian@gotinder.com>
2023-10-16 13:09:13 -0700

    Add LayoutCenter

---
+++ Sources/Layout/LayoutCenter.swift
@@ -0,0 +1,18 @@
+//
+//  All Contributions by Match Group
+//
+//  Copyright © 2023 Tinder (Match Group, LLC)
+//
+//  Licensed under the Match Group Modified 3-Clause BSD License.
+//  See https://github.com/Tinder/Layout/blob/main/LICENSE for license information.
+//
+
+import UIKit
+
+@preconcurrency
+@MainActor
+public protocol LayoutCenter: AnyObject {
+
+    var centerX: NSLayoutXAxisAnchor { get }
+    var centerY: NSLayoutYAxisAnchor { get }
+}
