2e76a2c1c8c76a63042a7cc2b627b03533a6014c
Garric Nahapetian <garric.nahapetian@gotinder.com>
2023-10-16 14:19:02 -0700

    Add UIEdgeInsets extension

---
+++ Sources/Layout/UIEdgeInsets.swift
@@ -0,0 +1,32 @@
+//
+//  All Contributions by Match Group
+//
+//  Copyright © 2023 Tinder (Match Group, LLC)
+//
+//  Licensed under the Match Group Modified 3-Clause BSD License.
+//  See https://github.com/Tinder/Layout/blob/main/LICENSE for license information.
+//
+
+import UIKit
+
+extension UIEdgeInsets {
+
+    internal var edgeType: CanonicalEdge.Type { CanonicalEdge.self }
+
+    internal init(_ inset: CGFloat) {
+        self.init(top: inset, left: inset, bottom: inset, right: inset)
+    }
+
+    internal func constant(for edge: CanonicalEdge) -> CGFloat {
+        switch edge {
+        case .top:
+            return top
+        case .bottom:
+            return -bottom
+        case .left:
+            return left
+        case .right:
+            return -right
+        }
+    }
+}
