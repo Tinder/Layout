78bf218896b236261bc7cf25dd849e4b4b9cb9d0
Garric Nahapetian <garric.nahapetian@gotinder.com>
2023-10-17 14:46:09 -0700

    Improve LayoutBuilderTests.swift

--- Tests/LayoutTests/LayoutBuilderTests.swift
+++ Tests/LayoutTests/LayoutBuilderTests.swift
@@ -14,52 +14,49 @@ import XCTest
 @MainActor
 final class LayoutBuilderTests: XCTestCase {
 
-    private final class LayoutItemMock: Equatable, LayoutItem {
-
-        nonisolated static func == (lhs: LayoutItemMock, rhs: LayoutItemMock) -> Bool {
-            lhs === rhs
-        }
-
-        let layoutItemView: UIView = .init()
-        let superviewConstraints: SuperviewConstraints = { _ in [] }
-    }
-
     func testLayoutBuilder() {
 
         // GIVEN
 
-        let layoutItem: LayoutItemMock = .init()
+        let layoutItem: LayoutItem = UIView()
 
         // THEN
 
         expect(LayoutBuilder.buildExpression(Optional<LayoutItem>.none)).to(beEmpty())
-        expect(LayoutBuilder.buildExpression(layoutItem) as? [LayoutItemMock]) == [layoutItem]
+        expect(LayoutBuilder.buildExpression(layoutItem) === [layoutItem]) == true
 
         expect(LayoutBuilder.buildExpression(Optional<[LayoutItem]>.none)).to(beEmpty())
-        expect(LayoutBuilder.buildExpression([layoutItem]) as? [LayoutItemMock]) == [layoutItem]
+        expect(LayoutBuilder.buildExpression([layoutItem]) === [layoutItem]) == true
 
         expect(LayoutBuilder.buildBlock()).to(beEmpty())
-        expect(LayoutBuilder.buildBlock([layoutItem]) as? [LayoutItemMock]) == [layoutItem]
+        expect(LayoutBuilder.buildBlock([layoutItem]) === [layoutItem]) == true
 
-        expect(LayoutBuilder.buildBlock([layoutItem], [layoutItem], [layoutItem]) as? [LayoutItemMock]) == [
+        expect(LayoutBuilder.buildBlock([layoutItem], [layoutItem], [layoutItem]) === [
             layoutItem,
             layoutItem,
             layoutItem
-        ]
+        ]) == true
 
         expect(LayoutBuilder.buildOptional(nil)).to(beEmpty())
-        expect(LayoutBuilder.buildOptional([layoutItem]) as? [LayoutItemMock]) == [layoutItem]
+        expect(LayoutBuilder.buildOptional([layoutItem]) === [layoutItem]) == true
 
-        expect(LayoutBuilder.buildEither(first: [layoutItem]) as? [LayoutItemMock]) == [layoutItem]
-        expect(LayoutBuilder.buildEither(second: [layoutItem]) as? [LayoutItemMock]) == [layoutItem]
+        expect(LayoutBuilder.buildEither(first: [layoutItem]) === [layoutItem]) == true
+        expect(LayoutBuilder.buildEither(second: [layoutItem]) === [layoutItem]) == true
 
-        expect(LayoutBuilder.buildArray([[layoutItem], [layoutItem], [layoutItem]]) as? [LayoutItemMock]) == [
+        expect(LayoutBuilder.buildArray([[layoutItem], [layoutItem], [layoutItem]]) === [
             layoutItem,
             layoutItem,
             layoutItem
-        ]
+        ]) == true
 
-        expect(LayoutBuilder.buildLimitedAvailability([layoutItem]) as? [LayoutItemMock]) == [layoutItem]
-        expect(LayoutBuilder.buildFinalResult([layoutItem]) as? [LayoutItemMock]) == [layoutItem]
+        expect(LayoutBuilder.buildLimitedAvailability([layoutItem]) === [layoutItem]) == true
+        expect(LayoutBuilder.buildFinalResult([layoutItem]) === [layoutItem]) == true
+    }
+}
+
+extension Array where Element == LayoutItem {
+
+    internal static func === (lhs: [LayoutItem], rhs: [LayoutItem]) -> Bool {
+        zip(lhs, rhs).allSatisfy { $0.0 === $0.1 }
     }
 }
