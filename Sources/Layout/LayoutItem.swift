513cd5d8e68af8f45413829d1bc72bf8e46f4cab
Eman Haroutunian <104394983+tinder-emanharoutunian@users.noreply.github.com>
2023-10-18 15:08:09 -0700

    Update Sources/Layout/LayoutItem.swift
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

--- Sources/Layout/LayoutItem.swift
+++ Sources/Layout/LayoutItem.swift
@@ -323,7 +323,7 @@ extension LayoutItem {
         }
     }
 
-    /// Constrains the view's edges to the superview's safeAreaGuide with directional `insets`
+    /// Constrains the view's directional edges to the superview's safe area with insets.
     ///
     /// - Parameters:
     ///   - insets: (optional) insets of view
