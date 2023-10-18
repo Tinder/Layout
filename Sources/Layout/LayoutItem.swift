5b86057d5ea268199db7473682dc9211265d5ee6
tinder-emanharoutunian <eman.haroutunian@gotinder.com>
2023-10-18 11:30:55 -0700

    Add toEdges(insets: priority:)

--- Sources/Layout/LayoutItem.swift
+++ Sources/Layout/LayoutItem.swift
@@ -283,6 +283,24 @@ extension LayoutItem {
         }
     }
 
+    /// Constrains the edges to the superview's canonical edges with `insets`
+    ///
+    /// - Parameters:
+    ///   - insets: (optional) insets of view
+    ///   - priority: (optional) priority of constraint
+    public func toEdges(
+        insets: UIEdgeInsets,
+        priority: UILayoutPriority = .required
+    ) -> LayoutItem {
+        addingSuperviewConstraints { layoutItem in
+            if let superview: UIView = layoutItem.layoutItemView.superview {
+                for edge: CanonicalEdge in insets.edgeType.allCases {
+                    layoutItem.constraint(to: edge, of: superview, insets: insets, priority: priority)
+                }
+            }
+        }
+    }
+
     /// Constrains the `bottom` to the bottom margin with a minimum bottom inset
     /// Useful for bottom-aligned elements on iPhoneX family devices that require
     /// an additional bottom margin on non-iPhoneX devices.
