5395439ee683d9f00adfd7a725d11d43dd5e7a02
Garric Nahapetian <garric.nahapetian@gotinder.com>
2023-10-19 17:32:35 -0700

    Add color name to test snapshots

---
+++ Tests/LayoutTests/Support/View.swift
@@ -0,0 +1,98 @@
+//
+//  All Contributions by Match Group
+//
+//  Copyright © 2023 Tinder (Match Group, LLC)
+//
+//  Licensed under the Match Group Modified 3-Clause BSD License.
+//  See https://github.com/Tinder/Layout/blob/main/LICENSE for license information.
+//
+
+import UIKit
+
+internal final class View: UIView {
+
+    override internal var description: String {
+        let identifier: String
+        if let accessibilityIdentifier, !accessibilityIdentifier.isEmpty {
+            identifier = "View \"\(accessibilityIdentifier)\""
+        } else {
+            identifier = "View"
+        }
+        return super
+            .description
+            .replacingOccurrences(of: String(reflecting: Self.self), with: identifier)
+            .split(separator: ";")[0...2]
+            .joined(separator: ";")
+            .appending(">")
+    }
+
+    internal convenience init(named name: String, with color: UIColor) {
+        self.init()
+        accessibilityIdentifier = name
+        backgroundColor = color
+    }
+}
+
+internal var pinkView: UIView {
+    View(named: "Pink", with: UIColor(red: 1, green: 0.176471, blue: 0.333333, alpha: 1))
+}
+
+internal var blackView: UIView {
+    View(named: "Black", with: .black)
+}
+
+internal var darkGrayView: UIView {
+    View(named: "DarkGray", with: .darkGray)
+}
+
+internal var lightGrayView: UIView {
+    View(named: "LightGray", with: .lightGray)
+}
+
+internal var whiteView: UIView {
+    View(named: "White", with: .white)
+}
+
+internal var grayView: UIView {
+    View(named: "Gray", with: .gray)
+}
+
+internal var redView: UIView {
+    View(named: "Red", with: .red)
+}
+
+internal var greenView: UIView {
+    View(named: "Green", with: .green)
+}
+
+internal var blueView: UIView {
+    View(named: "Blue", with: .blue)
+}
+
+internal var cyanView: UIView {
+    View(named: "Cyan", with: .cyan)
+}
+
+internal var yellowView: UIView {
+    View(named: "Yellow", with: .yellow)
+}
+
+internal var magentaView: UIView {
+    View(named: "Magenta", with: .magenta)
+}
+
+internal var orangeView: UIView {
+    View(named: "Orange", with: .orange)
+}
+
+internal var purpleView: UIView {
+    View(named: "Purple", with: .purple)
+}
+
+internal var brownView: UIView {
+    View(named: "Brown", with: .brown)
+}
+
+internal var clearView: UIView {
+    View(named: "Clear", with: .clear)
+}
