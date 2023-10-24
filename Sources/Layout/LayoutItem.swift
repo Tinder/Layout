a7a2aded43f23fedf9b5716ded625f6aafd9bc7f
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-10-24 12:26:13 -0700

    Add toSideMargins(inset:priority:) (#212)
    * Add toSideMargins(inset:priority:)
    
    * Improve documentation comments
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    * Record snapshots
    
    * Improve documentation comment
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    * Improve documentation comment
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    ---------
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

--- Sources/Layout/LayoutItem.swift
+++ Sources/Layout/LayoutItem.swift
@@ -465,6 +465,18 @@ extension LayoutItem {
         }
     }
 
+    /// Constrains the view's side edges to the superview's margins with an inset.
+    ///
+    /// - Parameters:
+    ///   - inset: (optional) The inset distance.
+    ///   - priority: (optional) The priority of constraints.
+    public func toSideMargins(
+        inset: CGFloat = 0,
+        priority: UILayoutPriority = .required
+    ) -> LayoutItem {
+        toMargins(canonical: [.left, .right], inset: inset, priority: priority)
+    }
+
     /// Constrains the `bottom` to the bottom margin with a minimum bottom inset
     /// Useful for bottom-aligned elements on iPhoneX family devices that require
     /// an additional bottom margin on non-iPhoneX devices.
