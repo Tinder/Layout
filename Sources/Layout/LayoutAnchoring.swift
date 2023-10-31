0614fcf4e33976ab794513811056abbcf4c715f3
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-10-30 18:25:26 -0700

    Move extension to NSLayoutYAxisAnchor.swift (#175)
    * Move extension to NSLayoutYAxisAnchor.swift
    
    * Move tests

--- Sources/Layout/LayoutAnchoring.swift
+++ Sources/Layout/LayoutAnchoring.swift
@@ -45,27 +45,3 @@ extension UILayoutGuide: LayoutBoundary, LayoutCenter, LayoutSize {
     public var width: NSLayoutDimension { widthAnchor }
     public var height: NSLayoutDimension { heightAnchor }
 }
-
-// swiftlint:disable function_default_parameter_at_end
-
-extension NSLayoutYAxisAnchor {
-
-    public func constraint(
-        is relation: NSLayoutConstraint.Relation = .equal,
-        to anchor: NSLayoutYAxisAnchor,
-        constant: CGFloat = 0
-    ) -> NSLayoutConstraint {
-        switch relation {
-        case .equal:
-            return constraint(equalTo: anchor, constant: constant)
-        case .greaterThanOrEqual:
-            return constraint(greaterThanOrEqualTo: anchor, constant: constant)
-        case .lessThanOrEqual:
-            return constraint(lessThanOrEqualTo: anchor, constant: constant)
-        @unknown default:
-            return constraint(equalTo: anchor, constant: constant)
-        }
-    }
-}
-
-// swiftlint:enable function_default_parameter_at_end
