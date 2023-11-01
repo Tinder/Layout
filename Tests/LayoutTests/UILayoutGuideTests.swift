5e2d0b64de622e283e2d15a10f354b0f51c86e6d
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-11-01 12:35:38 -0700

    Move UILayoutGuide and tests (#215)
    * Move UILayoutGuide and tests
    
    * Improve test names
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>
    
    ---------
    
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

---
+++ Tests/LayoutTests/UILayoutGuideTests.swift
@@ -0,0 +1,59 @@
+//
+//  All Contributions by Match Group
+//
+//  Copyright © 2023 Tinder (Match Group, LLC)
+//
+//  Licensed under the Match Group Modified 3-Clause BSD License.
+//  See https://github.com/Tinder/Layout/blob/main/LICENSE for license information.
+//
+
+@testable import Layout
+import Nimble
+import XCTest
+
+@MainActor
+final class UILayoutGuideTests: XCTestCase {
+
+    func testLayoutBoundary() {
+
+        // GIVEN
+
+        let guide: UILayoutGuide = .init()
+        let boundary: LayoutBoundary = guide
+
+        // THEN
+
+        expect(boundary.top) == guide.topAnchor
+        expect(boundary.bottom) == guide.bottomAnchor
+        expect(boundary.left) == guide.leftAnchor
+        expect(boundary.right) == guide.rightAnchor
+        expect(boundary.leading) == guide.leadingAnchor
+        expect(boundary.trailing) == guide.trailingAnchor
+    }
+
+    func testLayoutCenter() {
+
+        // GIVEN
+
+        let guide: UILayoutGuide = .init()
+        let center: LayoutCenter = guide
+
+        // THEN
+
+        expect(center.centerX) == guide.centerXAnchor
+        expect(center.centerY) == guide.centerYAnchor
+    }
+
+    func testLayoutSize() {
+
+        // GIVEN
+
+        let guide: UILayoutGuide = .init()
+        let size: LayoutSize = guide
+
+        // THEN
+
+        expect(size.width) == guide.widthAnchor
+        expect(size.height) == guide.heightAnchor
+    }
+}
