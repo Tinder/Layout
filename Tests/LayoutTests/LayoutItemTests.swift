abcd8f4f08797fd55a3f16535eace5fea5d10428
tinder-emanharoutunian <eman.haroutunian@gotinder.com>
2023-11-01 13:02:22 -0700

    Make toEdges(top:leading:bottom:trailing:priority) consistent

--- Tests/LayoutTests/LayoutItemTests.swift
+++ Tests/LayoutTests/LayoutItemTests.swift
@@ -456,9 +456,10 @@ final class LayoutItemTests: XCTestCase {
         assertLayout { view in
             view.layout {
 
-                // To Edges with Top, Leading, Bottom, Trailing
+                // To Edges with Top, Leading, Bottom, Trailing and Default Priority
 
                 pinkView
+                    .toEdges(top: 10, leading: 20, bottom: 10, trailing: 20, priority: .high)
                     .toEdges(top: 10, leading: 20, bottom: 10, trailing: 20)
 
                 // To Edges with Top, Leading, Bottom, Trailing with Priority
