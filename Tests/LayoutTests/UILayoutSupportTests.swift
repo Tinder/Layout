00e0f82f64658776d8f83c95be6438c447783b72
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-11-03 18:29:19 -0700

    Move UILayoutSupport tests (#229)
    * Move UILayoutSupport tests
    
    * Improve test name

---
+++ Tests/LayoutTests/UILayoutSupportTests.swift
@@ -0,0 +1,45 @@
+//
+//  All Contributions by Match Group
+//
+//  Copyright © 2023 Tinder (Match Group, LLC)
+//
+//  Licensed under the Match Group Modified 3-Clause BSD License.
+//  See https://github.com/Tinder/Layout/blob/main/LICENSE for license information.
+//
+
+@testable import Layout
+import Nimble
+import XCTest
+
+@MainActor
+final class UILayoutSupportTests: XCTestCase {
+
+    func testAnchors() {
+
+        // GIVEN
+
+        final class LayoutSupportMock: NSObject, UILayoutSupport {
+
+            var topAnchor: NSLayoutYAxisAnchor { view.top }
+            var bottomAnchor: NSLayoutYAxisAnchor { view.bottom }
+            var heightAnchor: NSLayoutDimension { view.height }
+
+            var length: CGFloat { fatalError("Not Used") }
+
+            private let view: UIView
+
+            init(view: UIView) {
+                self.view = view
+            }
+        }
+
+        let view: UIView = .init()
+        let layoutSupport: UILayoutSupport = LayoutSupportMock(view: view)
+
+        // THEN
+
+        expect(layoutSupport.top) == view.top
+        expect(layoutSupport.bottom) == view.bottom
+        expect(layoutSupport.height) == view.height
+    }
+}
